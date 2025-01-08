7e300a9d3d3bafa7babd2d82e7e759ba1b7d6193
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-01-07 23:16:08 -0800

    Added script for building a release
    Automatically makes a zip containing the resources folder, an elf
    binary, and an exe.

---
+++ build-release.sh
@@ -0,0 +1,8 @@
+cargo build --release
+cargo build --target x86_64-pc-windows-gnu --release
+mkdir Geometry-Rays
+mv ./target/x86_64-pc-windows-gnu/release/geometry-rays.exe ./Geometry-Rays
+mv ./target/release/geometry-rays ./Geometry-Rays
+cp -r ./Resources ./Geometry-Rays
+7z a -tzip Geometry-Rays.zip Geometry-Rays
+rm -rf ./Geometry-Rays
