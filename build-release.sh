a36bc3dbe532ac77c27dbbaca39e5ee55a10c1db
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-02-15 11:03:05 -0800

    Fixed build-release.sh
    Ok yeah until now it didnt include save-data lmao

--- build-release.sh
+++ build-release.sh
@@ -4,5 +4,6 @@ mkdir Geometry-Rays
 mv ./target/x86_64-pc-windows-gnu/release/geometry-rays.exe ./Geometry-Rays
 mv ./target/release/geometry-rays ./Geometry-Rays
 cp -r ./Resources ./Geometry-Rays
+cp -r ./save-data ./Geometry-Rays
 7z a -tzip Geometry-Rays.zip Geometry-Rays
 rm -rf ./Geometry-Rays
