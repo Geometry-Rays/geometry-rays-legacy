60b9b31fc9e2014f8a58b7f731180d72f4d05356
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-02-16 12:41:30 -0800

    Made points in player path get deleted after they are off screen
    I'll change this if I ever add a reverse trigger dw

--- src/main.rs
+++ src/main.rs
@@ -873,6 +873,17 @@ async fn main() {
                     player_path.push(Vector2 { x: 200.0 - world_offset, y: player.y + player_cam_y as f32 });
                 }
 
+                let mut pointindex: u32 = 0;
+                while pointindex < player_path.len().try_into().unwrap() {
+                    if player_path[pointindex as usize].x + world_offset < rl.get_screen_width() as f32 &&
+                    player_path[pointindex as usize].x + world_offset > -40.0 {
+                    } else {
+                        player_path.remove(pointindex as usize);
+                    }
+
+                    pointindex += 1
+                }
+
                 if kill_player {
                     game_state = GameState::GameOver;
                 }
