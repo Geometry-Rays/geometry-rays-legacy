f8ba2c9cc588a3eab21b76231b8d4c2e6b7b8fb1
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-03-21 18:42:06 -0700

    Fixed a bug
    My friend made an account with a blank user and a blank password and
    before this if you played one of the levels on that account the game
    would just crash. But its fixed.

--- src/funcs.rs
+++ src/funcs.rs
@@ -476,13 +476,26 @@ pub fn parse_level_download_response(
     online_level_data: &mut String
 ) {
     let level_download_result_parts: Vec<&str> = level_download_result.split(";;;;;").collect();
+    let level_download_result_parts_empty_user: Vec<&str> = level_download_result.split(";;;;;;").collect();
     let name_desc: Vec<&str> = level_download_result_parts[0].split(";").collect();
+
+    println!("{:?}", name_desc);
+    println!("{}", level_download_result);
 
     *online_level_name = name_desc[0].to_string();
     *online_level_desc = name_desc[1].to_string();
     *online_level_diff = name_desc[2].parse().unwrap();
-    *online_level_creator = name_desc[4].parse().unwrap();
-    *online_level_data = level_download_result_parts[1].to_string();
+    if name_desc.len() > 4 {
+        *online_level_creator = name_desc[4].parse().unwrap();
+    } else {
+        *online_level_creator = "".to_string();
+    }
+
+    if name_desc.len() > 4 {
+        *online_level_data = level_download_result_parts[1].to_string();
+    } else {
+        *online_level_data = level_download_result_parts_empty_user[1].to_string();
+    }
 
     if name_desc[3] == "0" {
         *online_level_rated = false
--- src/main.rs
+++ src/main.rs
@@ -254,7 +254,7 @@ async fn main() {
     let ship_power: f32 = 0.7;
     let ship_falling_speed: f32 = 0.5;
 
-    let version = "1.2";
+    let version = "1.21";
     let latest_version = std::sync::Arc::new(std::sync::Mutex::new(String::from("Loading...")));
     let mut not_done_yet_text = false;
     let mut show_debug_text = false;
