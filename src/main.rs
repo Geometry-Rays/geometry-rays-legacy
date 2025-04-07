6a0331f81bce928f7ec90eb80cc33f235ed15e03
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-04-07 13:13:11 -0700

    Every update is gonna have a patch right after the main update istg
    This time it was cuz stuff was very broken. Hopefully this makes stuff
    better

--- src/main.rs
+++ src/main.rs
@@ -436,7 +436,7 @@ async fn main() {
     let ship_power: f32 = 0.7;
     let ship_falling_speed: f32 = 0.5;
 
-    let version = "1.4";
+    let version = "1.41";
     let latest_version = std::sync::Arc::new(std::sync::Mutex::new(String::from("Loading...")));
     let mut not_done_yet_text = false;
     let mut show_debug_text = false;
