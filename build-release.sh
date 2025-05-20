8c0b5394539a911040f19eea0cb06c72b543b972
Lncvrt <lncvrtreal@gmail.com>
2025-05-20 16:37:59 -0700

    New version from https://github.com/Geometry-Rays/geometry-rays-fyre/commit/fd7f240155a7cce464c64a214291ea584afe43df (different than that commit but made for this repo)

--- build-release.sh
+++ build-release.sh
@@ -1,10 +1,30 @@
-cargo build --release
+#!/bin/bash
+set -euo pipefail
+
+echo Deleting old builds
+cargo clean
+
+echo Building for Linux
+cargo build --target x86_64-unknown-linux-gnu --release
+
+echo Building for Windows
 cargo build --target x86_64-pc-windows-gnu --release
+
+echo Creating folders
+rm -rf Geometry-Rays
 mkdir Geometry-Rays
+
+echo Moving executables
+mv ./target/x86_64-unknown-linux-gnu/release/geometry-rays ./Geometry-Rays
 mv ./target/x86_64-pc-windows-gnu/release/geometry-rays.exe ./Geometry-Rays
-mv ./target/release/geometry-rays ./Geometry-Rays
+
+echo Copying required folders
 cp -r ./Resources ./Geometry-Rays
 cp -r ./save-data ./Geometry-Rays
 cp -r ./Music ./Geometry-Rays
+
+echo Zipping the package
 7z a -tzip Geometry-Rays.zip Geometry-Rays
+
+echo Removing temporary directory
 rm -rf ./Geometry-Rays
